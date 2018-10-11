4e736d0824b94fd351bb34ccfa36d89dfef7f39b
rokridi <mohamed.aymen.landolsi@gmail.com>
2018-10-11 10:10:08 +0200

    Created project and added Alamofire Pod

---
+++ LeBonCoinTest/LeBonCoinTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LeBonCoinTest
+//
+//  Created by Mohamed Aymen Landolsi on 11/10/2018.
+//  Copyright © 2018 Rokridi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LeBonCoinTest/Pods/Target Support Files/Pods-LeBonCoinTest/Pods-LeBonCoinTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LeBonCoinTest : NSObject
+@end
+@implementation PodsDummy_Pods_LeBonCoinTest
+@end
